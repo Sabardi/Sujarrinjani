e58d75e3e9b4f17e2ab0f8eca03805d84d66b70a
tedi0609 <tedysahputra692@gmail.com>
2024-08-28 03:00:54 +0800

    penambahan instalasi laravel brezze

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,20 @@
 <?php
 
-use App\Http\Controllers\BookingController;
-use App\Http\Controllers\KategoriController;
-use App\Http\Controllers\PaymentController;
-use App\Http\Controllers\TourController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('kategori', KategoriController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-// Route to filter tours by category
-Route::get('/tours/category/{kategori}', [TourController::class, 'filterByCategory'])->name('tours.filterByCategory');
-
-
-Route::resource('tours', TourController::class);
-
-Route::resource('payments', PaymentController::class);
-
-Route::resource('bookings', BookingController::class);
+require __DIR__.'/auth.php';
