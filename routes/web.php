2dd1e69e699da92dba05464a8b9d7382689d0dcb
maganggetsurvey <magang@getsurvey.id>
2024-08-29 10:31:27 +0800

    perbaikan dashboard admin sujjarinjani

--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,12 @@ use App\Http\Controllers\ProfileController;
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
+Route::get('/', function () {
+    return view('admin.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
